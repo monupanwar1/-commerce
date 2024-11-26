40c30c8aa80fb30e59931922694140df4c8c0d34
panwarkunal <monupanwar5655@gmail.com>
2024-11-26 19:43:28 +0530

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navabr from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -21,17 +23,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#ffffff]`}
-        
-      >
-        <Navabr/>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#ffffff]`}>
+        <Navabr />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AiFillGithub,  AiOutlineLinkedin,  } from 'react-icons/ai';
+
+type Props = {}; // Define props type if necessary
+
+const Footer: React.FC<Props> = (props) => {
+  return (
+    <footer className="text-black flex flex-col  text-center py-4">
+      <p className='font-bold '>&copy; 2024 E-commerce. All rights reserved.</p>
+      <p> <p className="icons flex items-center justify-center text-4xl">
+        <AiFillGithub />
+        <AiOutlineLinkedin />
+      </p></p>
+    </footer>
+  );
+};
+
+export default Footer;
